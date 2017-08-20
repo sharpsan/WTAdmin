33e2aff68cde50324a807541b944f1e2ae6c83a0
sharpsan <gsharp.impact@gmail.com>
2017-08-20 02:23:54 -0400

    Making things more appy.

--- app/src/main/java/com/sharpsan/wtadmin/MainActivity.java
+++ app/src/main/java/com/sharpsan/wtadmin/MainActivity.java
@@ -2,7 +2,9 @@ package com.sharpsan.wtadmin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -19,8 +21,34 @@ public class MainActivity extends AppCompatActivity {
         myWebView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true); //enable JS
-        myWebView.loadUrl("http://toyzsa.com/admin"); //page to load
+        myWebView.loadUrl("http://toyzsa.com/admin/mobile"); //page to load
         myWebView.setWebViewClient(new WebViewClient()); //load all link clicks within app
+
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                myWebView.reload();
+                return true;
+            case R.id.action_settings:
+                //handle settings click
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
